2bc775baa6125fc6726bc94a3a8e317ed94840c8
Raghad Abdelhaq <raghadabdelhaq5@gmail.com>
2025-05-22 20:06:20 +0300

    update api.php file & adding usercontroller file

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\EventApiController;
+use App\Http\Controllers\UserController;
 
-
-Route::apiResource('events', EventApiController::class);
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('api')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);          
+    Route::get('/users/{id}', [UserController::class, 'show']);       
+    Route::post('/users', [UserController::class, 'store']);         
+    Route::put('/users/{id}', [UserController::class, 'update']);    
+    Route::delete('/users/{id}', [UserController::class, 'destroy']); 
+});
