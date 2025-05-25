4cb089976df84121fe8f8d3cd5e5e5191dccf4ba
Abdallah Saleh <s12324278@stu.najah.edu>
2025-05-25 19:34:37 +0300

    API for Events + EventCreations

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\EventApiController;
+use App\Http\Controllers\API\EventCreationApiController;
+
+Route::get('/event-creations/{event_id}/{user_id}', [EventCreationApiController::class, 'show']);
+Route::put('/event-creations/{event_id}/{user_id}', [EventCreationApiController::class, 'update']);
+Route::delete('/event-creations/{event_id}/{user_id}', [EventCreationApiController::class, 'destroy']);
+Route::get('/event-creations', [EventCreationApiController::class, 'index']);
+Route::post('/event-creations', [EventCreationApiController::class, 'store']);
+
 
 
 Route::apiResource('events', EventApiController::class);
